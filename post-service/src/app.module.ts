6a87468dcd300c18f1a2f3b33defdd3904148630
Sotatek-PhongBui <phong.bui@sotatek.com>
2025-07-16 17:03:23 +0700

    add post and comment module

--- post-service/src/app.module.ts
+++ post-service/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post/post.module';
+import { CommentController } from './comment/comment.controller';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -13,8 +16,10 @@ import { ConfigModule } from '@nestjs/config';
       },
     }),
     PrismaService,
+    PostModule,
+    CommentModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, CommentController],
   providers: [AppService],
 })
 export class AppModule {}
