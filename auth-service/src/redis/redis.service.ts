288089911f82bde4dbc17003b6545eedae11d442
xinchaoduyanh <duyanh19122k3@gmail.com>
2025-07-25 14:47:33 +0700

    Update redis.service.ts

--- auth-service/src/redis/redis.service.ts
+++ auth-service/src/redis/redis.service.ts
@@ -73,23 +73,23 @@ export class RedisService implements OnModuleDestroy {
   // Tạo OTP mới, xóa OTP cũ nếu có, đảm bảo không trùng toàn hệ thống
   async createForgotOtp(email: string, purpose: OtpPurpose, ttl: number) {
     // Xóa OTP cũ nếu có
-    await this.redisClient.del(`forgot_otp:${email}:${purpose}`);
+    await this.redisClient.del(`otp:${email}:${purpose}`);
     let otp: string;
     do {
       otp = Math.floor(100000 + Math.random() * 900000).toString();
-    } while (await this.redisClient.exists(`forgot_otp_code:${otp}`));
+    } while (await this.redisClient.exists(`otp_code:${otp}`));
     const data = {
       otp,
       status: OTP_STATUS.CREATED,
       createdAt: new Date().toISOString(),
     };
     await this.redisClient.set(
-      `forgot_otp:${email}:${purpose}`,
+      `otp:${email}:${purpose}`,
       JSON.stringify(data),
       'EX',
       ttl,
     );
-    await this.redisClient.set(`forgot_otp_code:${otp}`, email, 'EX', ttl);
+    await this.redisClient.set(`otp_code:${otp}`, email, 'EX', ttl);
     return otp;
   }
 
@@ -99,17 +99,17 @@ export class RedisService implements OnModuleDestroy {
     data: any,
     ttlSeconds: number,
   ) {
-    const key = `forgot_otp:${email}:${otp}`;
+    const key = `otp:${email}:${otp}`;
     await this.redisClient.set(key, JSON.stringify(data), 'EX', ttlSeconds);
   }
 
   async getOtp(email: string, purpose: OtpPurpose) {
-    const val = await this.redisClient.get(`forgot_otp:${email}:${purpose}`);
+    const val = await this.redisClient.get(`otp:${email}:${purpose}`);
     return val ? JSON.parse(val) : null;
   }
 
   async updateOtpStatus(email: string, purpose: OtpPurpose, status: OtpStatus) {
-    const key = `forgot_otp:${email}:${purpose}`;
+    const key = `otp:${email}:${purpose}`;
     const val = await this.redisClient.get(key);
     if (!val) return null;
     const data = JSON.parse(val);
@@ -123,12 +123,12 @@ export class RedisService implements OnModuleDestroy {
   }
 
   async deleteOtp(email: string, purpose: OtpPurpose, otp: string) {
-    await this.redisClient.del(`forgot_otp:${email}:${purpose}`);
-    await this.redisClient.del(`forgot_otp_code:${otp}`);
+    await this.redisClient.del(`otp:${email}:${purpose}`);
+    await this.redisClient.del(`otp_code:${otp}`);
   }
 
   async checkOtpExists(email: string, otp: string) {
-    const key = `forgot_otp:${email}:${otp}`;
+    const key = `otp:${email}:${otp}`;
     const exists = await this.redisClient.exists(key);
     return exists === 1;
   }
